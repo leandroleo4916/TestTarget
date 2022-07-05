907962e6fec6effbc2773ab3f0bb556e46d76f38
Leandro Leo <70068568+leandroleo4916@users.noreply.github.com>
2022-07-04 23:49:56 -0300

    Update TesteFibonacci.kt

--- TesteFibonacci.kt
+++ TesteFibonacci.kt
@@ -1,25 +1,26 @@
 fun main() {
-    val numE = 378
+    
+    val numIn = 378
     var numI = 0
     var numJ = 1
     var aux = 0
     var value = false
 
     while (!value) {
-        if (numE > numJ) {
+        if (numIn > numJ) {
             aux = numI + numJ
             numI = numJ
             numJ = aux
-            println("$numJ")
         }else {
             value = true
         }
     }
     
-    if (numJ == numE){
-        println("$numE faz parte do Fibonacci")
+    if (numJ == numIn){
+        println("$numIn faz parte do Fibonacci")
     }
     else {
-        println("$numE não faz parte do Fibonacci")
+        println("$numIn não faz parte do Fibonacci")
     }
+    
 }
